818b4c38a1e7bf9a92d5517a479809b54956b957
Simon Kramer <simonmaximiliankramer13gmail.com>
2023-09-29 18:38:12 +0200

    fixed a syntax error

--- utils/commands.py
+++ utils/commands.py
@@ -2,7 +2,7 @@ from maya import cmds
 from maya.api import OpenMaya
 
 
-def create_single_joint()
+def create_single_joint():
 	""" Creates a single joint and clears the current selection. """
 	cmds.select(cl=True)
 	jnt = cmds.joint()
