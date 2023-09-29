6adfb05feb7378c1271da2953b7289acbec83ae6
Simon Kramer <simonmaximiliankramer13gmail.com>
2023-09-29 19:34:28 +0200

    improved the orient joint chain script

--- utils/orient_joints.py
+++ utils/orient_joints.py
@@ -8,12 +8,12 @@ reload(utils.helper)
 
 
 def orient_joint_chain(joint_chain=None, aim_vec=[1,0,0], up_vec=[0,0,1]):
-    """ """
+    """ Orients the first three joints of joint chain, so that they are oriented perpendicular to
+        the plane they define. Subsequent joints stay untoched. The axis that is perpendicular to
+        the construction plane is neither the aim vector nor the up vector, but the third vector. """
 
     if joint_chain is None:
         joint_chain = utils.helper.select_by_root_joint()
-
-    chain_len = len(joint_chain)
 
     for n, jnt in enumerate(joint_chain):
         children = cmds.listRelatives(children=True)
@@ -21,17 +21,19 @@ def orient_joint_chain(joint_chain=None, aim_vec=[1,0,0], up_vec=[0,0,1]):
             for child in children:
                 cmds.parent(child, world=True)
 
-        if n == chain_len-2:
-            cnst = cmds.aimConstraint(joint_chain[n+1], jnt, worldUpType='object', aimVector=aim_vec, upVector=up_vec, worldUpObject=joint_chain[0], worldUpVector=up_vec, mo=False)[0]
+        if n == 0:
+            cnst = cmds.aimConstraint(joint_chain[n+1], jnt, worldUpType='object', aimVector=aim_vec,
+                upVector=up_vec, worldUpObject=joint_chain[n+2], worldUpVector=up_vec, mo=False)[0]
             cmds.delete(cnst)
             cmds.makeIdentity(jnt, apply=True)
-        elif n == chain_len-1:
+        elif n == 1:
+            cnst = cmds.aimConstraint(joint_chain[n+1], jnt, worldUpType='object', aimVector=aim_vec,
+                upVector=up_vec, worldUpObject=joint_chain[0], worldUpVector=up_vec, mo=False)[0]
+            cmds.delete(cnst)
+            cmds.makeIdentity(jnt, apply=True)
+        elif n == 2:
             cmds.makeIdentity(jnt, apply=True)
             cmds.joint(jnt, edit=True, orientation=[0,0,0])
-        else:
-            cnst = cmds.aimConstraint(joint_chain[n+1], jnt, worldUpType='object', aimVector=aim_vec, upVector=up_vec, worldUpObject=joint_chain[n+2], worldUpVector=up_vec, mo=False)[0]
-            cmds.delete(cnst)
-            cmds.makeIdentity(jnt, apply=True)
 
         if children:
             for child in children:
